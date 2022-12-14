18f664a60a45c57a61a095a3f392b9bcb3d1ef75
kknyapple <kknyapple@gmail.com>
2022-12-15 01:50:44 +0900

    feat: 숫자 야구 게임하기

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,5 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 const Model = require("../models/Model");
 const View = require("../views/View");
+const { ERROR } = require("../constant/error");
 
 class Controller {
   view;
@@ -8,10 +11,85 @@ class Controller {
   constructor(view, model) {
     this.view = view;
     this.model = model;
+    this.result = "";
+  }
+
+  isThreeDigitNumber(number) {
+    if (!isNaN(number)) return number.length === 3 ? true : false;
+    else return false;
+  }
+
+  isOneToNine(number) {
+    return /^[1-9]+$/.test(number);
+  }
+
+  isDifferentDigitNumber(number) {
+    const numArr = [...String(number)];
+
+    return new Set(numArr).size == numArr.length ? true : false;
+  }
+
+  validateInputNumber(number) {
+    if (
+      this.isThreeDigitNumber(number) &&
+      this.isOneToNine(number) &&
+      this.isDifferentDigitNumber(number)
+    ) {
+      return true;
+    } else {
+      throw new Error(ERROR.INPUT.NUMBER);
+    }
+  }
+
+  compareNumber(input, computer) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      (input[i] === computer[i] && strike++) ||
+        (input[i] !== computer[i] && input.includes(computer[i]) && ball++);
+    }
+    this.model.setResult([strike, ball]);
+  }
+
+  setResultMessage([strike, ball]) {
+    (this.result = strike === 3 && "3스트라이크") ||
+      (this.result =
+        strike !== 0 && ball !== 0 && `${ball}볼 ${strike}스트라이크`) ||
+      (this.result = strike === 0 && ball !== 0 && `${ball}볼`) ||
+      (this.result = strike !== 0 && ball === 0 && `${strike}스트라이크`) ||
+      (this.result = strike === 0 && ball === 0 && "낫싱");
+  }
+
+  getResultMessage() {
+    return this.result;
+  }
+
+  startGame() {
+    this.view.printStart();
+    this.model.setComputerNumber();
+    console.log(this.model.getComputerNumber());
+  }
+
+  playGame() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      this.model.setInputNumber(input);
+      console.log(this.model.getInputNumber());
+      this.validateInputNumber(input);
+      this.compareNumber(
+        this.model.getComputerNumber(),
+        this.model.getInputNumber()
+      );
+      this.setResultMessage(this.model.getResult());
+      MissionUtils.Console.print(this.getResultMessage());
+
+      this.playGame();
+    });
   }
 
   game() {
-    this.view.printStart();
+    this.startGame();
+    this.playGame();
   }
 }
 
--- src/views/View.js
+++ src/views/View.js
@@ -1,8 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-const Model = require("../models/Model");
-const { ERROR } = require("../constant/error");
-
 class View {
   model;
 
@@ -10,40 +7,8 @@ class View {
     this.model = model;
   }
 
-  isThreeDigitNumber(number) {
-    if (!isNaN(number)) return number.length === 3 ? true : false;
-    else return false;
-  }
-
-  isOneToNine(number) {
-    return /^[1-9]+$/.test(number);
-  }
-
-  isDifferentDigitNumber(number) {
-    const numArr = [...String(number)];
-
-    return new Set(numArr).size == numArr.length ? true : false;
-  }
-
-  validateInputNumber(number) {
-    if (
-      this.isThreeDigitNumber(number) &&
-      this.isOneToNine(number) &&
-      this.isDifferentDigitNumber(number)
-    ) {
-      return true;
-    } else {
-      throw new Error(ERROR.INPUT.NUMBER);
-    }
-  }
-
   printStart() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
-      this.model.setInputNumber(input);
-      console.log(this.model.getInputNumber());
-      this.validateInputNumber(input);
-    });
   }
 }
 
